d767ce1c9d2de3efe55aec3c8993c14322337f29
junaid-lahore <53553257+junaid-lahore@users.noreply.github.com>
2025-12-16 12:17:49 +0500

    Delete frontend/.next directory

--- frontend/.next/static/chunks/pages/index-1ee8a25318da2ae9.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[405],{3319:function(n,u,_){(window.__NEXT_P=window.__NEXT_P||[]).push(["/",function(){return _(6302)}])},6302:function(n,u,_){"use strict";_.r(u);var t=_(1472);u.default=t.Z}},function(n){n.O(0,[603,352,472,888,774,179],function(){return n(n.s=3319)}),_N_E=n.O()}]);
